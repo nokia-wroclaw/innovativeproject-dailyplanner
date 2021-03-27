ab39c8162efd51a14f76c82f206558de4b96c453
TomekPaluch <tomektalaga@interia.pl>
2021-03-27 14:25:50 +0100

    change App  from class component to functional component

--- django_react_proj/users-fe/src/App.js
+++ django_react_proj/users-fe/src/App.js
@@ -2,15 +2,14 @@ import React, { Component, Fragment } from "react";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Header />
-        <Home />
-      </Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Home />
+    </Fragment>
+  );
+};
+
 
 export default App;
