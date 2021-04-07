40eaf636837c4317fd4e5fe7e80358f9a70b1417
mankowski1damian <mankowski.damian2@gmail.com>
2021-04-07 10:58:33 +0200

    Created UsersListRow file and conneted the TASKBUTTON with the functionality

--- django_react_proj/users-fe/src/components/TaskButtonModal/TaskButtonModal.js
+++ django_react_proj/users-fe/src/components/TaskButtonModal/TaskButtonModal.js
@@ -1,49 +1,40 @@
-import React, { useState, Fragment } from "react";
+import React, {useState} from "react";
 import { Modal, ModalHeader, Button, ModalFooter } from "reactstrap";
-import styles from './TaskButtonModal.module.css';
+import styles from "./TaskButtonModal.module.css";
 
 const TaskButton = (props) => {
-  const [modalFlag, setModalFlag] = useState(
-        false
-  )
-
-  const [taskFlag, setTaskFlag] = useState(
-    false
-  )
+  const [modalFlag, setModalFlag] = useState(false);
 
   const toggle = () => {
-    setModalFlag(previous => (
-       !previous
-     ));
+    setModalFlag((previous) => !previous);
   };
-  
-  const handlerDeleteUser = async id => {
-    try {
-      await props.deleteUser(id)
-      toggle() 
-    } catch (error) {
-      console.log(error)
-    } 
-   };
 
-  const toggleTaskFlag = () => {
-    setTaskFlag(previous => (
-        !previous
-      ));
+  const handlerDeleteUser = async (id) => {
+    try {
+      await props.deleteUser(id);
+      toggle();
+    } catch (error) {
+      console.log(error);
+    }
   };
-   
+
+  // const toggleTaskFlag = () => {
+  //   setTaskFlag(previous => (
+  //       !previous
+  //     ));
+  // };
+
   return (
     <>
       <Button
-        color = {taskFlag ? 'success': 'danger'}
-        onClick = {() => toggleTaskFlag()}>
+        color={props.taskFlag ? "success" : "danger"}
+        onClick={() => props.ontoggleTaskFlag()}
+      >
         TASKBUTTON
       </Button>
 
       <Modal isOpen={modalFlag} toggle={toggle}>
-        <ModalHeader toggle={toggle}>
-          Czy chcesz usunąć zadanie?
-        </ModalHeader>
+        <ModalHeader toggle={toggle}>Czy chcesz usunąć zadanie?</ModalHeader>
 
         <ModalFooter>
           <Button type="button" onClick={() => toggle()}>
@@ -63,4 +54,3 @@ const TaskButton = (props) => {
 };
 
 export default TaskButton;
-
---
+++ django_react_proj/users-fe/src/components/UsersListRow/UsersListRow.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import { API_URL } from "../../constants";
+import NewUserModal from "../NewUserModal/NewUserModal";
+import ConfirmRemovalModal from "../ConfirmRemovalModal/ConfirmRemovalModal";
+import TaskButton from "../TaskButtonModal/TaskButtonModal";
+import styles from "../UserList/UserList.module.css";
+
+
+const UsersListRow = ({user,resetState}) => {
+    const [taskFlag, setTaskFlag] = useState(
+      false
+  )
+  
+  const ontoggleTaskFlag = () => {
+    setTaskFlag(previous => (
+        !previous
+      ));
+  };
+  
+  const deleteUser = async(id) => {
+    try{
+      await axios.delete(`${API_URL}${id}/`)
+      resetState()
+    } catch (error){
+      console.log(error)
+    }
+  };
+  
+  return(
+    <tr className = {taskFlag ? styles.ButtonTrue : styles.ButtonFalse} key={user.id}>
+      <td>{user.name}</td>
+      <td className={styles.Svg}>{user.password}</td>
+      <td>{user.registrationDate}</td>
+      <td align="center">
+        <TaskButton
+          id = {user.id}
+          deleteUser = {deleteUser}
+          taskFlag = {taskFlag}
+  
+          ontoggleTaskFlag = {ontoggleTaskFlag}
+        />
+        &nbsp;&nbsp;
+        <NewUserModal
+          create={false}
+          user={user}
+          resetState={resetState}
+        />
+        &nbsp;&nbsp;
+        <ConfirmRemovalModal
+          id= {user.id}
+          deleteUser={deleteUser}
+        />
+      </td>
+    </tr>
+  )
+  }
+
+  export default UsersListRow;
+
