1f89c19048d6d16469b06aa0a97e2dfec9d6380e
Nobleman <urzedowski.artur@gmail.com>
2021-04-12 19:40:31 +0200

    Clean up js files, delete <Fragment> wrappers and unused imports

--- django_react_proj/users-fe/src/components/NewUserModal/NewUserModal.js
+++ django_react_proj/users-fe/src/components/NewUserModal/NewUserModal.js
@@ -1,4 +1,4 @@
-import React, {  useState,Fragment } from "react";
+import React, {  useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
 import NewUserForm from "../NewUserForm/NewUserForm";
 import styles from './NewUserModal.module.css';
