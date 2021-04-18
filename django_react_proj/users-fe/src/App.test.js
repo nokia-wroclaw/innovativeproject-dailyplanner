8240c27a4d5127caace877ef9f1557ffe1354a16
TomekPaluch <tomektalaga@interia.pl>
2021-04-18 23:15:51 +0200

    changing

--- django_react_proj/users-fe/src/App.test.js
+++ django_react_proj/users-fe/src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders welcome message', () => {
+test('renders learn react link', () => {
   render(<App />);
-  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
