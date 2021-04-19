20604ff83f0c475e614ad5ad2811fdd8d8f9c17e
TomekPaluch <tomektalaga@interia.pl>
2021-04-19 11:02:42 +0200

    Fix App.test.js filev2

--- django_react_proj/users-fe/src/App.test.js
+++ django_react_proj/users-fe/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.debug("learn react");
+  screen.debug();
+  const linkElement = screen.getByText('learn react');
   expect(linkElement).toBeInTheDocument();
 });
