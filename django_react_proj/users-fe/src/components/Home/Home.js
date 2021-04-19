f35d2e41c987401dfe571320147f0ff995e7bc32
garnek69 <lgancarz1@wp.pl>
2021-04-19 19:50:27 +0200

    added new component RealTime with funcionality + css model

--- django_react_proj/users-fe/src/components/Home/Home.js
+++ django_react_proj/users-fe/src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "reactstrap";
 import axios from "axios";
-
 import styles from './Home.module.css';
 import UserList from "../UserList/UserList";
 import NewUserModal from "../NewUserModal/NewUserModal";
 import { API_URL } from "../../constants";
+import Time from "../RealTime/RealTime"
 
 const Home = () => {
   const [users, setUsers] = useState();
@@ -37,6 +37,9 @@ const Home = () => {
             resetState={getUsers} 
           />
         </Col>
+      </Row>
+      <Row>
+          <Time/>
       </Row>
     </Container>
   );
