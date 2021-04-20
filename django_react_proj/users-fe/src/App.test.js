285e24473631013d4730a1248889841a8c012105
TomekPaluch <tomektalaga@interia.pl>
2021-04-20 16:43:51 +0200

    Changing travis file

--- django_react_proj/users-fe/src/App.test.js
+++ django_react_proj/users-fe/src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  screen.debug();
-  const linkElement = screen.getByText('learn react');
+  const linkElement = screen.getByText('DailyPlanner');
   expect(linkElement).toBeInTheDocument();
 });
