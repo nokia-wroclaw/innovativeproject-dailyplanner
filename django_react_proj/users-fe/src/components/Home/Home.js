2b591e84cbac071dc38348f60476d07f0494e653
garnek69 <lgancarz1@wp.pl>
2021-04-21 12:43:35 +0200

    fix

--- django_react_proj/users-fe/src/components/Home/Home.js
+++ django_react_proj/users-fe/src/components/Home/Home.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "reactstrap";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import axios from 'axios';
 import styles from './Home.module.css';
-import UserList from "../UserList/UserList";
-import NewUserModal from "../NewUserModal/NewUserModal";
-import { API_URL } from "../../constants";
-import Time from "../RealTime/RealTime"
+import UserList from '../UserList/UserList';
+import NewUserModal from '../NewUserModal/NewUserModal';
+import { API_URL } from '../../constants';
+import Time from '../RealTime/RealTime';
 
 const Home = () => {
   const [users, setUsers] = useState();
@@ -31,7 +31,7 @@ const Home = () => {
         </Col>
       </Row>
       <Row>
-          <Time/>
+        <Time />
       </Row>
     </Container>
   );
--- django_react_proj/users-fe/src/components/RealTime/RealTime.js
+++ django_react_proj/users-fe/src/components/RealTime/RealTime.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import styles from "./RealTime.module.css"
-import { format } from 'date-fns'
-  
+import { format } from 'date-fns';
+import styles from './RealTime.module.css';
+
 const Time = () => {
-    const [time, setTime] = React.useState(0);
-    const currentCallback = () => {
+  const [time, setTime] = React.useState(0);
+  const currentCallback = () => {
     const date = new Date();
-    const dateformat = format(date, "HH:mm:ss");
+    const dateformat = format(date, 'HH:mm:ss');
     setTime(dateformat);
-    };
-    React.useEffect(() => {
+  };
+  React.useEffect(() => {
     const handle = setInterval(currentCallback, 1000);
     return () => clearInterval(handle);
-    }, []);
-    return (
-    <div className={styles.Time}>{time}</div>
-    );
+  }, []);
+  return <div className={styles.Time}>{time}</div>;
 };
-  
+
 export default Time;
