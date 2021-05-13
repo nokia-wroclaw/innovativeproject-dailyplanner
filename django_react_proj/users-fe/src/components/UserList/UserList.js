aad739ae4b5165ec260349840b8093f8c06e00f1
TomekPaluch <tomektalaga@interia.pl>
2021-05-13 20:32:47 +0200

    Cleanup

--- django_react_proj/users-fe/src/components/UserList/UserList.js
+++ django_react_proj/users-fe/src/components/UserList/UserList.js
@@ -7,7 +7,7 @@ import TaskButton from '../TaskButtonModal/TaskButtonModal';
 import NewUserModal from '../NewUserModal/NewUserModal';
 import ConfirmRemovalModal from '../ConfirmRemovalModal/ConfirmRemovalModal';
 import { API_URL } from '../../constants';
-import styles from '../UsersListRow/UsersListRow.module.css';
+import styles from './UserList.module.css';
 
 const { Column } = Table;
 
@@ -80,28 +80,6 @@ const UserList = ({ users = [], resetState }) => (
       render={(value, user) => <Actions user={user} resetState={resetState} />}
     />
   </Table>
-  // <Table dark>
-  //   <thead>
-  //     <tr>
-  //       <th>Task name</th>
-  //       <th>Description</th>
-  //       <th>Task Time</th>
-  //       <th>Task Type</th>
-  //       <th />
-  //     </tr>
-  //   </thead>
-  //   <tbody>
-  //     {users.length === 0 ? (
-  //       <tr>
-  //         <td colSpan="6" align="center">
-  //           <b>You haven't scheduled any task!</b>
-  //         </td>
-  //       </tr>
-  //     ) : (
-  //       users.map((user) => <UsersListRow key={user.id} user={user} resetState={resetState} />)
-  //     )}
-  //   </tbody>
-  // </Table>
 );
 UserList.propTypes = {
   users: PropTypes.array,
