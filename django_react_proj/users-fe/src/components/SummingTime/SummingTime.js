45b87711eb326718dc7071be8f3cf0e957f0bbbb
garnek69 <lgancarz1@wp.pl>
2021-05-13 22:08:00 +0200

    fix

--- django_react_proj/users-fe/src/components/SummingTime/SummingTime.js
+++ django_react_proj/users-fe/src/components/SummingTime/SummingTime.js
@@ -1,5 +1,5 @@
 import { Pie } from '@ant-design/charts';
-import { Card, Col } from 'antd';
+import { Card } from 'antd';
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './SummingTime.module.css';
