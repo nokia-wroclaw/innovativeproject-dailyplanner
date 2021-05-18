bca354f26a90472f31733dd92c2c12f480fe6d4a
TomekPaluch <tomektalaga@interia.pl>
2021-05-18 20:50:06 +0200

    Changing yml file, add procfile

--- django_react_proj/django_react_proj/settings.py
+++ django_react_proj/django_react_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,4 +132,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 STATIC_URL = '/static/'
 
-
+django_heroku.settings(locals())
