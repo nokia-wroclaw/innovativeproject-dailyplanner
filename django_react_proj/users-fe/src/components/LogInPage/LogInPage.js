a0c357acd2124c1fcc55b54c21223ec6c6b41403
Arczi4 <urzedowski.artur@gmail.com>
2021-06-13 22:38:43 +0200

    change log in page

--- django_react_proj/users-fe/src/components/LogInPage/LogInPage.js
+++ django_react_proj/users-fe/src/components/LogInPage/LogInPage.js
@@ -1,5 +1,5 @@
 import { React, useContext } from 'react';
-import { Button, Card, Row } from 'antd';
+import { Button, Row, Col } from 'antd';
 import { GithubOutlined, GoogleOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import PropTypes from 'prop-types';
@@ -18,30 +18,56 @@ const LogInPage = ({ history }) => {
     setEmail(res.email);
   };
   return (
-    <Card title="Log In" bordered={false} style={{ textAlign: 'center' }}>
-      <Row justify="center">
-        <Button
-          onClick={() => handleOnClickGithub(githubProvider)}
-          icon={<GithubOutlined />}
-          color="#8c8c8c"
-          size="large"
-          style={{ background: '#bfbfbf' }}
-        >
-          GitHub
-        </Button>
+    <div
+      style={{
+        position: 'absolute',
+        left: '47%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <Row>
+        <Col>
+          <Row>
+            <img
+              style={{ width: 430, height: 320, borderRadius: 10 }}
+              alt="Team chaos"
+              src="https://image.freepik.com/darmowe-wektory/miejsce-pracy-studenta-lub-biznesmena-w-pokoju_1284-13477.jpg"
+            />
+          </Row>
+          <Row>
+            <a href="https://pl.freepik.com/wektory/tlo">
+              Tło plik wektorowy utworzone przez upklyak - pl.freepik.com
+            </a>
+          </Row>
+        </Col>
+        <Col style={{ marginLeft: 100, marginTop: 50 }}>
+          <h2>Log in</h2>
+          <Row style={{ marginBottom: 40, marginTop: 20 }}>
+            <Button
+              onClick={() => handleOnClickGithub(githubProvider)}
+              icon={<GithubOutlined />}
+              color="#8c8c8c"
+              size="large"
+              style={{ background: '#bfbfbf' }}
+            >
+              GitHub
+            </Button>
+          </Row>
+          <Row>
+            <Button
+              onClick={() => handleOnClickGithub(googleProvider)}
+              icon={<GoogleOutlined />}
+              color="#f5222d"
+              size="large"
+              style={{ background: '#096dd9' }}
+            >
+              Google
+            </Button>
+          </Row>
+        </Col>
       </Row>
-      <Row justify="center">
-        <Button
-          onClick={() => handleOnClickGithub(googleProvider)}
-          icon={<GoogleOutlined />}
-          color="#f5222d"
-          size="large"
-          style={{ background: '#096dd9' }}
-        >
-          Google
-        </Button>
-      </Row>
-    </Card>
+    </div>
   );
 };
 
